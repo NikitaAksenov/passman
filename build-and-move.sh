d4abfc6603b06c89ed6978df8e99cc174685c184
Nikita Aksenov <naksenov96@gmail.com>
2025-08-01 15:56:11 +0300

    Added script for building and installing

---
+++ build-and-move.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# Run go build
+echo "Building Go project..."
+go build -o passman.exe ./cmd/passman-cli
+
+# Get the name of the binary (assumes current directory is the project root)
+BINARY_NAME=$(basename "$PWD")
+
+# Check if GOPATH is set
+if [ -z "$GOPATH" ]; then
+    echo "GOPATH is not set. Using default GOPATH..."
+    GOPATH=$(go env GOPATH)
+fi
+
+# Move the binary to $GOPATH/bin
+DEST="$GOPATH/bin/$BINARY_NAME"
+echo "Moving binary to $DEST"
+mv "$BINARY_NAME" "$DEST"
+
+echo "Build and move completed successfully!"
