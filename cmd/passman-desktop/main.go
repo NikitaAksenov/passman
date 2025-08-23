526d6990abb2ce6b55c076517eb85ffc168819e2
NikitaAksenov <naksenov96@gmail.com>
2025-08-23 22:43:59 +0300

    Changed new target entry validation

--- cmd/passman-desktop/main.go
+++ cmd/passman-desktop/main.go
@@ -145,15 +145,19 @@ func (da *desktopApp) NewAddEntryWindow() fyne.Window {
 	addEntryWindow := da.FyneApp.NewWindow("New entry")
 
 	addEntryWindow.Resize(fyne.NewSize(300.0, 150.0))
+
 	addEntryWindow_Entry := widget.NewEntry()
 	addEntryWindow_Entry.PlaceHolder = "Target"
+
 	addEntryWindow_FirstPass := widget.NewEntry()
 	addEntryWindow_FirstPass.Password = true
 	addEntryWindow_FirstPass.PlaceHolder = "Enter password..."
+
 	addEntryWindow_SecondPass := widget.NewEntry()
 	addEntryWindow_SecondPass.Password = true
 	addEntryWindow_SecondPass.PlaceHolder = "Re-enter password..."
-	addEntryWindow_EnterButton := widget.NewButton("Enter", func() {
+
+	addEntryWindow_EnterButton := widget.NewButton("Enter target", func() {
 		if addEntryWindow_FirstPass.Text != addEntryWindow_SecondPass.Text {
 			fmt.Println("Pass's must match")
 			return
@@ -187,13 +191,33 @@ func (da *desktopApp) NewAddEntryWindow() fyne.Window {
 		da.mainWindow.Canvas().Content().Refresh()
 	})
 	addEntryWindow_EnterButton.Disable()
-	addEntryWindow_Entry.OnChanged = func(s string) {
-		addEntryWindow_EnterButton.Disable()
 
-		if len(s) > 0 {
-			addEntryWindow_EnterButton.Enable()
+	enterButtonValidate := func(s string) {
+		addEntryWindow_EnterButton.Enable()
+		addEntryWindow_EnterButton.SetText("Enter")
+
+		if len(addEntryWindow_Entry.Text) < 3 {
+			addEntryWindow_EnterButton.Disable()
+			addEntryWindow_EnterButton.SetText("Target too short")
+			return
+		}
+
+		if len(addEntryWindow_FirstPass.Text) < 3 {
+			addEntryWindow_EnterButton.Disable()
+			addEntryWindow_EnterButton.SetText("Password too short")
+			return
+		}
+
+		if addEntryWindow_FirstPass.Text != addEntryWindow_SecondPass.Text {
+			addEntryWindow_EnterButton.Disable()
+			addEntryWindow_EnterButton.SetText("Passwords don't match")
+			return
 		}
 	}
+	addEntryWindow_Entry.OnChanged = enterButtonValidate
+	addEntryWindow_FirstPass.OnChanged = enterButtonValidate
+	addEntryWindow_SecondPass.OnChanged = enterButtonValidate
+
 	addEntryWindow.SetContent(container.NewBorder(
 		nil, addEntryWindow_EnterButton, nil, nil, container.NewVBox(addEntryWindow_Entry, addEntryWindow_FirstPass, addEntryWindow_SecondPass),
 	))
