0dc7a46dc9e59e8ee7a03b59c01afe1af3d14528
NikitaAksenov <naksenov96@gmail.com>
2025-08-25 21:55:34 +0300

    Added flag to suppress terminal window

--- build-desktop.sh
+++ build-desktop.sh
@@ -5,6 +5,6 @@ set -e
 
 # Run go build
 echo "Building passman-desktop..."
-go build -ldflags="-X 'github.com/NikitaAksenov/passman/internal/app.appConfiguration=prod'" -o passman.exe ./cmd/passman-desktop
+go build -ldflags="-X 'github.com/NikitaAksenov/passman/internal/app.appConfiguration=prod' -H windowsgui" -o passman.exe ./cmd/passman-desktop
 
 echo "Build passman-desktop completed successfully!"
